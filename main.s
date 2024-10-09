0b779c348d81f6172ef5547a964876579c9125c0
Georgijs Onischenko <xel27@inbox.lv>
2024-10-09 20:39:45 +0200

    Add response message, improve code quality

--- main.s
+++ main.s
@@ -16,27 +16,6 @@
 #define SOCK_STREAM 1
 #define INTERNET_PROTOCOL 0
 
-// Socket address, internet style.
-/*
-	struct sockaddr_in {
-		__uint8_t       sin_len;
-		sa_family_t     sin_family;
-		in_port_t       sin_port;
-		struct  in_addr sin_addr;
-		char            sin_zero[8];
-	};
-*/
-.macro sockaddr_in_asm name_sin_len, name_sin_fam, name_sin_port, name_sin_zero, name_sin_addr, value_sin_len, value_sin_fam, value_sin_port, value_sin_addr
-	\name_sin_len:  .byte \value_sin_len
-	\name_sin_fam:  .byte \value_sin_fam
-	\name_sin_port: .hword \value_sin_port
-	\name_sin_addr: .word \value_sin_addr
-	\name_sin_zero: .dword 0
-.endm
-
-#define sockaddr_in(name, _sin_len, _sin_fam, _sin_port, _sin_addr)	\
-sockaddr_in_asm name.sin_len, name.sin_fam, name.sin_port, name.sin_addr, name.sin_zero, _sin_len, _sin_fam, _sin_port, _sin_addr \
-
 .macro load_addr register, addr
 	adrp \register, \addr@PAGE
 	add \register, \register, \addr@PAGEOFF
@@ -66,20 +45,9 @@ sockaddr_in_asm name.sin_len, name.sin_fam, name.sin_port, name.sin_addr, name.s
 	bl _listen
 .endm
 
-// int     accept(int, struct sockaddr * __restrict, socklen_t * __restrict)
+// int accept(int, struct sockaddr * __restrict, socklen_t * __restrict)
 .macro accept sockfd, addr, addrlen
 	func_call _accept, \sockfd, \addr, \addrlen
-.endm
-
-// ssize_t send(int sockfd, const void buf[.len], size_t len, int flags);
-.macro send lbl_sockfd, lbl_msg, lbl_msg_len, flags
-	load_addr X0, \lbl_sockfd
-	ldr X0, [X0]
-	load_addr X1, \lbl_msg
-	load_addr X2, \lbl_msg_len
-	ldr X2, [X2]
-	mov X3, \flags
-	bl _send
 .endm
 
 // int close(int fd);
@@ -108,16 +76,16 @@ _load_socket:
 	socket AF_INET, SOCK_STREAM, #0
 	cmp X0, #0
 	b.lt _error
-	adrp X1, sockfd@PAGE
-	add X1, X1, sockfd@PAGEOFF
-	str X0, [X1]
+	adrp X12, sockfd@PAGE
+	add X12, X12, sockfd@PAGEOFF
+	str X0, [X12]
+	ldr X11, [X12]
 
 _bind_socket:
 	write STDOUT, binding_socket_msg, binding_socket_msg_len
-	load_addr X0, sockfd
-	ldr X0, [X0]
+	mov X0, X11
 	load_addr X1, address.sin_len
-	load_addr X2, sockaddr_in_len
+	load_addr X2, address.struct_size
 	ldr X2, [X2]
 	bind X0, X1, X2
 	cmp X0, #0
@@ -125,8 +93,7 @@ _bind_socket:
 
 _listen_socket:
 	write STDOUT, listen_socket_msg, listen_socket_msg_len
-	load_addr X0, sockfd
-	ldr X0, [X0]
+	mov X0, X11
 	listen X0, #3
 	cmp X0, #0
 	b.lt _error
@@ -134,35 +101,67 @@ _listen_socket:
 _loop:
 _accept_connections:
 	write STDOUT, accept_connections_msg, accept_connections_msg_len
-	load_addr X0, sockfd
-	ldr X0, [X0]
+	mov X0, X11
 	load_addr X1, address.sin_len
-	load_addr X2, sockaddr_in_len
+	load_addr X2, address.struct_size
 	bl _accept
 	cmp X0, #0
 	b.lt _error
-// _send_response:
-// 	send
+	load_addr X1, new_sockfd
+	str X0, [X1]
+
+_send_response:
+	load_addr X0, new_sockfd
+	ldr X0, [X0]
+	write X0, server_response_msg, server_response_msg_len
 	b _exit
 
-_error:
-	write STDOUT, error_msg, error_msg_len
+_close_fds:
+	stp X29, X30, [sp, #-16]!
 	load_addr X0, sockfd
 	ldr X0, [X0]
 	bl _close
+	load_addr X0, new_sockfd
+	ldr X0, [X0]
+	bl _close
+	ldp X29, X30, [sp], #16
+	ret
+
+_error:
+	write STDOUT, error_msg, error_msg_len
+	bl _close_fds
 	exit #-1
 
 _exit:
 	write STDOUT, ok_msg, ok_msg_len
-	load_addr X0, sockfd
-	ldr X0, [X0]
-	bl _close
+	bl _close_fds
 	exit #0
 
 .data
-sockfd: .word 0
-sockaddr_in(address, 0, AF_INET, PORT, INADDR_ANY)
-sockaddr_in_len: .word 16
+.align 8
+// Socket fds
+sockfd:     .xword 0
+new_sockfd: .xword 0
+
+// Structs
+// Socket address, internet style.
+/*
+	struct sockaddr_in {
+		__uint8_t       sin_len;
+		sa_family_t     sin_family;
+		in_port_t       sin_port;
+		struct  in_addr sin_addr;
+		char            sin_zero[8];
+	};
+*/
+address.sin_len:     .byte 0
+address.sin_fam:     .byte AF_INET
+address.sin_port:    .hword PORT
+address.sin_addr:    .word INADDR_ANY
+address.sin_zero:    .dword 0
+address.struct_size: .word 16
+
+// Messages
 loading_socket_msg: .asciz "INFO: Loading Socket...\n"
 loading_socket_msg_len: .word loading_socket_msg_len - loading_socket_msg
 binding_socket_msg: .asciz "INFO: Binding Socket...\n"
@@ -171,6 +170,8 @@ listen_socket_msg: .asciz "INFO: Listening Socket...\n"
 listen_socket_msg_len: .word listen_socket_msg_len - listen_socket_msg
 accept_connections_msg: .asciz "INFO: Accepting connections...\n"
 accept_connections_msg_len: .word accept_connections_msg_len - accept_connections_msg
+server_response_msg: .asciz "Hello from ARM64 ASM!\n"
+server_response_msg_len: .word server_response_msg_len - server_response_msg
 error_msg: .asciz "INFO: Error!\n"
 error_msg_len: .word error_msg_len - error_msg
 ok_msg: .asciz "INFO: OK!\n"
